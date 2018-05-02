dcaa3d7b12e9ff2aff8606b582e8841beca9520f
chinnonae <chinnonae@gmail.com>
2018-05-02 21:59:57 +0700

    change list user url

--- routes/api.php
+++ routes/api.php
@@ -28,4 +28,4 @@ Route::delete('/task', 'TasksController@delete');
 Route::post('/login', 'Auth\LoginController@login');
 
 Route::middleware('auth:api')->post('/user', 'UserController@store');
-Route::get('/user', 'UserController@index');
+Route::get('/users', 'UserController@index');
